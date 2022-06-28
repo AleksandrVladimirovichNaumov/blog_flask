c94aecffb15c4940bc290710744f436bd4bf3f4c
alex <skisk8fmx@gmail.com>
2022-06-28 13:28:49 +0300

    new post

--- my_blog/blog/routes.py
+++ my_blog/blog/routes.py
@@ -1,7 +1,69 @@
-from flask import render_template, Blueprint
+from flask import (render_template, url_for, flash,
+                   redirect, request, abort, Blueprint)
+from flask_login import current_user, login_required
+from my_blog import db
+from my_blog.models import Post
+from my_blog.blog.forms import PostForm
 
 blog = Blueprint('blog', __name__)
 
+
 @blog.route("/blog")
-def blog_main():
-    return render_template('blog.html')
+def allpost():
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(Post.date_posted.desc()). \
+        paginate(page=page, per_page=5)
+    return render_template('blog.html', posts=posts)
+
+
+@blog.route("/new_post", methods=['GET', 'POST'])
+@login_required
+def new_post():
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(title=form.title.data, content=form.content.data,
+                    author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Ваш пост создан!', 'success')
+        return redirect(url_for('blog.allpost'))
+    return render_template('create_post.html',
+                           title='Новый пост', form=form, legend='Новый пост')
+
+
+@blog.route("/post/<int:post_id>")
+def post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('post.html', title=post.title, post=post)
+
+
+@blog.route("/post/<int:post_id>/update", methods=['GET', 'POST'])
+@login_required
+def update_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    form = PostForm()
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.commit()
+        flash('Ваш пост обновлен!', 'success')
+        return redirect(url_for('posts.post', post_id=post.id))
+    elif request.method == 'GET':
+        form.title.data = post.title
+        form.content.data = post.content
+    return render_template('create_post.html', title='Обновление поста',
+                           form=form, legend='Обновление поста')
+
+
+@blog.route("/post/<int:post_id>/delete", methods=['POST'])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash('Ваш пост был удален!', 'success')
+    return redirect(url_for('posts.allpost'))
--- my_blog/models.py
+++ my_blog/models.py
@@ -32,3 +32,4 @@ class Post(db.Model):
 
     def __repr__(self):
         return f"Запись('{self.title}', '{self.date_posted}')"
+
